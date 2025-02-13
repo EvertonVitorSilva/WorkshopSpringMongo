3d693e50687cee52b6d6b2ee4d90496bf6c3bb30
Everton Vitor da Silva <everton.vitordasilva@outlook.com>
2025-02-12 22:18:54 -0300

    Start a project with Springboot and MongoDB

---
+++ src/test/java/com/evertonsilva/workshop/WorkshopSpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evertonsilva.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
